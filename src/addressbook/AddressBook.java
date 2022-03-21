ae6c41304cbd48deedc427ad1a2751c7e3f7ed6a
kumarb97 <kumarbamankar@gmail.com>
2022-03-21 20:13:20 +0530

    Use case seven check duplicate entry added

---
+++ src/addressbook/AddressBook.java
@@ -0,0 +1,195 @@
+package addressbook;
+import java.util.*;
+public class AddressBook {
+	/**
+     * Creating a scanner object to read inputs from user
+     */
+    private static Scanner scanner = new Scanner(System.in);
+    /**
+     * Creating a Map object with String type as key and List type as value
+     */
+    private  static Map<String, List<Contact>> addressBook = new HashMap<>();
+
+    /**
+     * Creating addNewContact method to add contacts into list
+     * @param scanner - taking scanner object
+     */
+    public static void addNewContact(Scanner scanner) {
+
+        System.out.println("Please enter the contact type  1.Office Contact\n2.Personal Contact");
+        String contactType = scanner.nextInt() == 1 ? "Office" : "Personal";
+        /**
+         * Taking all the address book inputs from user and storing them in respective fields as below
+         */
+        System.out.println("Please enter the details :");
+        System.out.println("First Name :");
+        String firstName = scanner.next();
+        System.out.println("Last Name :");
+        String lastName = scanner.next();
+        System.out.println("Address :");
+        String address = scanner.next();
+        System.out.println("City :");
+        String city = scanner.next();
+        System.out.println("State :");
+        String state = scanner.next();
+        System.out.println("Zip :");
+        long zip = scanner.nextLong();
+        System.out.println("Phone Number :");
+        long phoneNumber = scanner.nextLong();
+        System.out.println("Email :");
+        String email = scanner.next();
+
+        /**
+         * Creating Contact object and passing all the details as params
+         */
+        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        List<Contact> tempContactList = null;
+        /**
+         * If there is no existing entry for given contact type then create new array list
+         */
+        if (addressBook.get(contactType) == null) {
+            tempContactList = new ArrayList<>();
+        } else{
+            tempContactList = addressBook.get(contactType);
+        }
+        if (!tempContactList.contains(contact)) {
+            tempContactList.add(contact);
+            addressBook.put(contactType, tempContactList);
+        }
+    }
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book program...!");
+        String userChoice;
+        do {
+            readUserInput(scanner);
+            System.out.println("Do you want to continue(Y/N) ?");
+            userChoice = scanner.next();
+        } while (userChoice.equalsIgnoreCase("Y"));
+        System.out.println("Thank you!");
+    }
+
+    /**
+     * @param scanner - taking scanner object
+     * Method for giving the user to select he option and perform acc to it
+     */
+    private static void readUserInput(Scanner scanner) {
+        System.out.println("Please select one option");
+        System.out.println("1. Create new contact \n2. Edit contact \n3. List contacts \n4. Delete contact ");
+        int userChoice = scanner.nextInt();
+        switch (userChoice) {
+            case 1:
+                addNewContact(scanner);
+                System.out.println("Contact added successfully!");
+                break;
+            case 2:
+                editContact();
+                break;
+            case 3:
+                listContacts();
+                break;
+            case 4:
+                deleteContact();
+                break;
+            default:
+                System.out.println("Invalid option. Please select valid");
+        }
+    }
+
+    /**
+     * Method for deleting the contact for the given name as input
+     */
+    private static void deleteContact() {
+        System.out.println("Please enter the contact type  1.Office Contact\n2.Personal Contact");
+        String contactType = scanner.nextInt() == 1 ? "Office" : "Personal";
+        List<Contact> contactList = addressBook.get(contactType);
+        if (contactList == null) {
+            System.out.println("No such contact book exists!");
+        } else {
+            System.out.println("Please enter the name of the person u want to delete :");
+            String contactName = scanner.next();
+            Iterator<Contact> iterator = contactList.iterator();
+            while (iterator.hasNext()) {
+                Contact contact = iterator.next();
+                if (contactName.equals(contact.getFirstName())) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+    /**
+     * Displaying the list
+     */
+    private static void listContacts () {
+        for(Map.Entry<String, List<Contact>> entry : addressBook.entrySet()) {
+            System.out.println(entry.getKey()+"\n"+entry.getValue());
+        }
+    }
+
+    /**
+     * This method is used to give the user edit option
+     */
+    private static void editContact () {
+        System.out.println("Please enter the contact type  1.Office Contact\n2.Personal Contact");
+        String contactType = scanner.nextInt() == 1 ? "Office" : "Personal";
+        List<Contact> contactList = addressBook.get(contactType);
+        if (contactList == null) {
+            System.out.println("No such contact book exists!");
+        } else {
+            System.out.println("Please enter the name of the person u want to edit :");
+            String contactName = scanner.next();
+            Optional<Contact> optionalContact = contactList.stream().filter(contact1 -> contact1.getFirstName().equals(contactName)).findFirst();
+            if (!optionalContact.isPresent()) {
+                System.out.println("Sorry could not find the contact with the given name");
+                return;
+            }
+            Contact contact = optionalContact.get();
+            System.out.println("Please select the field you want to edit ");
+            System.out.println("1.Lastname  2.Address  3.City  4.State  5.Zip  6.Phone number  7.email ");
+            int userChoice = scanner.nextInt();
+            switch (userChoice) {
+                case 1:
+                    System.out.println("Please enter new last name :");
+                    String lastName = scanner.next();
+                    contact.setLastName(lastName);
+                    break;
+                case 2:
+                    System.out.println("Please enter new address :");
+                    String address = scanner.next();
+                    contact.setAddress(address);
+                    break;
+                case 3:
+                    System.out.println("Please enter new city :");
+                    String city = scanner.next();
+                    contact.setCity(city);
+                    break;
+                case 4:
+                    System.out.println("Please enter new state :");
+                    String state = scanner.next();
+                    contact.setState(state);
+                    break;
+                case 5:
+                    System.out.println("Please enter new zip :");
+                    long zip = scanner.nextLong();
+                    contact.setZip(zip);
+                    break;
+                case 6:
+                    System.out.println("Please enter new phone number :");
+                    long phoneNumber = scanner.nextLong();
+                    contact.setPhoneNumber(phoneNumber);
+                    break;
+                case 7:
+                    System.out.println("Please enter new email :");
+                    String email = scanner.next();
+                    contact.setEmail(email);
+                    break;
+                default:
+                    System.out.println("Invalid option. Please enter valid !");
+                    scanner.close();
+            }
+            listContacts();
+        }
+
+	}
+
+}
---
+++ src/addressbook/Contact.java
@@ -0,0 +1,140 @@
+package addressbook;
+
+public class Contact {
+	/**
+     * Declaring the variables for the details of a contact
+     */
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private long zip;
+    private long phoneNumber;
+    private String email;
+
+    /**
+     * Creating a parameterised constructor to take all the below params
+     * @param firstName
+     * @param lastName
+     * @param address
+     * @param city
+     * @param state
+     * @param zip
+     * @param phoneNumber
+     * @param email
+     */
+    Contact(String firstName, String lastName, String address, String city,
+            String state, long zip, long phoneNumber, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    /**
+     * creating getter and setter methods for each variable
+     */
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getZip() {
+        return zip;
+    }
+
+    public void setZip(long zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Overriding equals method for comparing the first name
+     * @param o - of type Object
+     * @return true or false
+     */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return this.firstName.equalsIgnoreCase(contact.getFirstName());
+    }
+
+    /**
+     * Overriding toString method to print the list in a format
+     * @return - list
+     */
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phoneNumber=" + phoneNumber +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
